bab4d997891cfc389e223f241933e09bdb707b92
mehgellan <baumegg@gmail.com>
2016-06-29 20:32:32 -0700

    create fn forshakespeare challenge

--- challenges/bottles-of-beer-song.js
+++ challenges/bottles-of-beer-song.js
@@ -28,5 +28,7 @@ var bottlesOfBeer = function(x) {
       console.log(i + " bottles of beer!");
       console.log("Take one down and pass it around,");
       console.log([i - 1] + " bottles of beer on the wall...");
+      console.log(" ");
   }
+
 };
